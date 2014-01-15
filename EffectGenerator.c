f46569fa5f97a41230d7b545d288b4ba99d0150e
Oliver Lea <olilea3@gmail.com>
2014-01-15 15:27:54 +0000

    Added averaging and changed buffer looping method

--- EffectGenerator.c
+++ EffectGenerator.c
@@ -5,29 +5,39 @@
 #include "adcInit.h"
 #include "dacInit.h"
 
-#define ADC_SAMPLE_RATE	200000
+#define ADC_SAMPLE_RATE	44000
 #define BUFFER_SIZE 4096
 
 uint16_t sampleBuffer[BUFFER_SIZE];
-uint16_t *sampleP = &sampleBuffer;
+uint16_t *sampleP = sampleBuffer;
 
 void ADC_IRQHandler(void) {
 
 	*sampleP = ADC_ChannelGetData(LPC_ADC, ADC_CHANNEL_4);
 
-	dacSetValue(*sampleP);
+	if (sampleP < &(sampleBuffer[2])) {
+		dacSetValue(*sampleP);
+	} else {
+		dacSetValue(((*sampleP + *(sampleP - 1) + *(sampleP - 2)) / 3) >> 2);
+	}
 
 	//Select next buffer location based on previous location
 	//When at the end of the buffer, loop round to the beginning next
-	sampleP = sampleBuffer + ((uint16_t)++sampleP % BUFFER_SIZE);
+	//sampleP = sampleBuffer + ((uint16_t)++sampleP % BUFFER_SIZE);
 
-	/*
-	if (sampleP < sampleBuffer[BUFFER_SIZE - 1]) {
+
+	if (sampleP < &(sampleBuffer[BUFFER_SIZE - 1])) {
 		sampleP++;
 	} else {
 		sampleP = sampleBuffer;
 	}
-	*/
+}
+
+// Assuming the given value fits criteria, function 
+// called and gain applied
+uint16_t upwardCompression(uint16_t analogueValue) {
+
+	
 }
 
 int main(void) {
