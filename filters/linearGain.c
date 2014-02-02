c32044a17cf9f688758cd645eca7d9b8fe28987b
Oliver Lea <olilea3@gmail.com>
2014-02-02 15:20:04 +0000

    Added linearGain init method

--- filters/linearGain.c
+++ filters/linearGain.c
@@ -1,23 +1,35 @@
 
 #include "lpc_types.h"
 
+#include "../filter.h"
 #include "linearGain.h"
+#include "../global.h"
 #include "../debug.h"
 
 // Simple linear gain filter that multiplies the input
 // by the gain about zero volts which is 2.04V with bias
 uint32_t linearGainF(uint32_t sample, float parameters[5]) {
 
-	float gainMultiplier = parameters[0];
+	float gain = parameters[0];
 
 	if (sample > 2048) {
-		return (sample * gainMultiplier);
+		return (sample * gain);
 	} else {
-		return (sample - ((sample * gainMultiplier) - sample));
+		return (sample - ((sample * gain) - sample));
 	}
 }
 
 void printLinearGainF(float parameters[5]) {
 
 	printfToTerminal("Linear Gain:\n\r\t\tGain Multiplier: %f\n\r\r", parameters[0]);
+}
+
+Filter *createLinearGainF(float gain) {
+
+	Filter *linGainFilter = createFilterS(&linearGainF, gain,
+			UNUSED, UNUSED, UNUSED, UNUSED);
+
+	(linGainFilter->sfilter)->printFilter = &printLinearGainF;
+
+	return linGainFilter;
 }
--- filters/linearGain.h
+++ filters/linearGain.h
@@ -5,4 +5,6 @@ uint32_t linearGainF(uint32_t sample, float parameters[5]);
 
 void printLinearGainF(float parameters[5]);
 
+Filter *createLinearGainF(float gain);
+
 #endif
