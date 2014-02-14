3af8c99afada1529394245d6ff635e443804b996
DazB <db819@york.ac.uk>
2014-02-14 16:11:12 +0000

    Added own fix to envelope follower. Balls = To wall

--- filters/envFollower.c
+++ filters/envFollower.c
@@ -15,15 +15,27 @@
 static uint32_t envWindow[WINDOW_SIZE];
 static uint8_t oldestElem;
 
+uint32_t envelope = 0;
+
 // See and reference:
 // http://www.kvraudio.com/forum/viewtopic.php?p=5178628
+//http://www.musicdsp.org/archive.php?classid=2
 
 // ONLY ONE ENVELOPE FOLLOWER CAN BE IN THE FILTER CHAIN AT ANY
 // ONE TIME
 uint32_t envFollowerF(uint32_t sample, SFilter *filter) {
 
-	//envWindow[oldestElem] = sample;
+	//attack, follow rising wave
+  if (sample > envelope){
+    envelope = ATTACK_COEFF * (envelope - sample) + sample;
+  else
+  //release, follow falling wave
+    envelope = RELEASE_COEFF  * (envelope - sample) + sample;
+  return envelope
 
+
+/*
+  //envWindow[oldestElem] = sample;
 	if (oldestElem == (WINDOW_SIZE - 1)) {
 		oldestElem = 0;
 	} else {
@@ -54,6 +66,8 @@ uint32_t envFollowerF(uint32_t sample, SFilter *filter) {
 //	printfToTerminal("Intial: %d, final: %d\n\r", sample, max);
 
 	return max;
+*/
+
 }
 
 void printEnvFollowerF(SFilter *filter) {
@@ -70,7 +84,7 @@ Filter *createEnvFollowerF(float attack_ms, float release_ms) {
 			UNUSED, UNUSED, UNUSED);
 
 	(envFollow->sfilter)->scratch[0] = exp(log(0.01) / (attack_ms * ADC_SAMPLE_RATE * 0.001));
-	(envFollow->sfilter)->scratch[1] = exp(log(0.01) / (release_ms * ADC_SAMPLE_RATE * 0.001));
+	(envFollow->sfilter)->scratch[1] = exp(0.01 / (release_ms * ADC_SAMPLE_RATE * 0.001));
 
 	return envFollow;
 }
