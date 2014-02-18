b6ed2fa97946e6e6ab9fd7152e55c011ac43cbb6
DazB <db819@york.ac.uk>
2014-02-18 16:46:08 +0000

    Added band pass filter

--- Makefile
+++ Makefile
@@ -34,7 +34,7 @@ LDFLAGS+=-L$(CMSIS)/lib -lDriversLPC17xxgnu
 
 EXECNAME	= bin/LIBS
 
-OBJ		= main.o userInterface.o debug.o adcInit.o timerInit.o dacInit.o filterChain.o filter.o filters/envFollower.o filters/flange.o filters/linearGain.o filters/delay.o filters/echo.o filters/reverb.o filters/tremelo.o filters/lowPass.o filters/highPass.o
+OBJ		= main.o userInterface.o debug.o adcInit.o timerInit.o dacInit.o filterChain.o filter.o filters/envFollower.o filters/flange.o filters/linearGain.o filters/delay.o filters/echo.o filters/reverb.o filters/tremelo.o filters/lowPass.o filters/highPass.o filters/bandPass.o
 
 all: 	main
 	@echo "Build finished"
---
+++ filters/bandPass.c
@@ -0,0 +1,34 @@
+#include "lpc_types.h"
+
+#include "../filter.h"
+#include "bandPass.h"
+#include "../global.h"
+#include "../debug.h"
+
+uint32_t bandPassF(uint32_t sample, SFilter *filter) {
+  float cutOffLow = filter->parameters[0];
+  float cutOffHigh = filter->parameters[1];
+  //if sample is above cutOffLow and below cutOffHigh then return
+  if (sample > cutOffLow){
+    if (sample < cutOffHigh){
+      return sample;
+    } 
+    else return 0; //return nothing
+  }
+  //else return nothing
+  else return 0;
+}
+
+void printBandPassF(SFilter *filter) {
+
+	printfToTerminal("Band Pass:\n\r\t\tCut Off Low: %f\n\r\r %f\n\r\t\tCut Off High: %f\n\r\r",
+			filter->parameters[0], filter->parameters[1]);
+}
+
+Filter *createBandPassF(float cutOffLow, float cutOffHigh) {
+
+	Filter *bandPassFilter = createFilterS(&bandPassF, &printBandPassF, cutOffLow, cutOffHigh,
+			UNUSED, UNUSED, UNUSED);
+
+	return bandPassFilter;
+}
