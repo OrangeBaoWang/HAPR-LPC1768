b2651cce9d7305dbd5d1f32d08e3cccd13b34092
Oliver Lea <olilea3@gmail.com>
2014-03-08 17:29:54 +0000

    Commented global.h

--- global.h
+++ global.h
@@ -1,3 +1,28 @@
+// Created by Oliver Lea - 21/01/2014
+
+// O-Edit: Added the global declaration of ADC_SAMPLE_RATE - 22/01/2014
+
+// O-Edit: Added globally visible sampleBuffer and sampleP for use throughout
+//		the program, and defined UNUSED as 0 - 02/02/2014
+
+// O-Edit: Defined AMPLITUDE_MAX to be 4096 (highest input from the ADC) and
+//		defined AMPLITUDE_PIVOT to be AMPLITUDE_MAX/2 - 24/02/2014
+
+// O-Edit: Defined SAMPLE_RATE_US which is the sample rate converted into the
+//		time between each sample in microseconds. Also added a globally visible
+//		passThrough boolean which defines the state (on/off) of pass-through - 24/02/2014
+
+// O-Edit: Added a new global varaible (wdtCounter) which counts the time between
+//		each watchdog feed and the end of the sampling ISR. Also added a new constant
+//		called WDT_TIMEOUT_US which is the timeout time of the watchdog timer in
+//		microseconds - 25/02/2014
+
+// D-Edit: Added new global variable (infraMix) that is used to determine whether infrared
+//		mixing is on or off - 25/02/2014
+
+// O-Edit: Added new global variable terminalBuffer that is used to store ASCII chars
+//		read from the terminal - 04/03/2014
+
 #ifndef __GLOBAL_H__
 #define __GLOBAL_H__
 
