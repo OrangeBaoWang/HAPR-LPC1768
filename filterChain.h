86778fe0bba7ab21cddb54a127d04200432945ea
Oliver Lea <olilea3@gmail.com>
2014-03-09 13:56:48 +0000

    Update filterChain.h

--- filterChain.h
+++ filterChain.h
@@ -1,5 +1,30 @@
 // Created by Oliver Lea 17/01/2014
 
+// O-Edit: Defined the FilterNode struct that is the top-level componenet of the filter
+//	chain - 18/01/2014
+
+// O-Edit: Added apply filters function that takes the current chain of filters
+//	and the sample to be filtered, and returns the post-processed sample - 19/01/2014
+
+// O-Edit: Added dequeuing parallel filters to the dequeue() function. Also added a
+//	filterEq() function that tests if two SFilters are equivalent - 23/01/2014
+
+// O-Edit: Began work on a printQueue() function that would allow the filter chain
+//	to be printed from the UI. Work in progress - 29/01/2014
+
+// O-Edit: Modified filterEq(), applyFilters() and the (unfinished) printQueue()
+//	functions to support a change in how filter parameters are passed (Used to be
+//	passed as two floats, but are now passed as an array of five floats) - 31/01/2014
+
+// O-Edit: dequeueByIndex() function added that removes a filter from the filter
+//	chain according to its index - 12/02/2014
+
+// O-Edit: enqueueByIndex() function added that adds a filter to the filter chain
+//	in a specified position given by the index passed to the function - 17/02/2014
+
+// O-Edit: Added freeNode() function (frees previously malloc'd space now unused by
+//	old filters) and dequeueAll() function (empties the filter chain) - 18/02/2014
+
 #ifndef __FILTERCHAIN_H__
 #define __FILTERCHAIN_H__
 
